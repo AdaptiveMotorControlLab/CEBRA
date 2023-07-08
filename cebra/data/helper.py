a9761ce57f5de998bd18f1fb236b6f004721771f
Rodrigo <gonlairo@gmail.com>
2023-07-08 11:00:54 +0200

    add helper function into cebra.data.helper

--- cebra/data/helper.py
+++ cebra/data/helper.py
@@ -18,7 +18,39 @@ import numpy as np
 import numpy.typing as npt
 import scipy.linalg
 import torch
+import cebra.data
 
+def get_loader_options(dataset: cebra.data.Dataset) -> List[str]:
+    """Return all possible dataloaders for the given dataset."""
+
+    loader_options = []
+    if isinstance(dataset, cebra.data.SingleSessionDataset):
+        mixed = True
+        if dataset.continuous_index is not None:
+            loader_options.append(cebra.data.ContinuousDataLoader)
+        else:
+            mixed = False
+        if dataset.discrete_index is not None:
+            loader_options.append(cebra.data.DiscreteDataLoader)
+        else:
+            mixed = False
+        if mixed:
+            loader_options.append(cebra.data.MixedDataLoader)
+    elif isinstance(dataset, cebra.data.MultiSessionDataset):
+        mixed = True
+        if dataset.continuous_index is not None:
+            loader_options.append(cebra.data.ContinuousMultiSessionDataLoader)
+        else:
+            mixed = False
+        if dataset.discrete_index is not None:
+            pass  # not implemented yet
+        else:
+            mixed = False
+        if mixed:
+            pass  # not implemented yet
+    else:
+        raise TypeError(f"Invalid dataset type: {dataset}")
+    return loader_options
 
 def _require_numpy_array(array: Union[npt.NDArray, torch.Tensor]):
     if not isinstance(array, np.ndarray):
--- cebra/helper.py
+++ cebra/helper.py
@@ -21,40 +21,11 @@ from typing import List
 import requests
 
 import cebra.data
-
-
-def get_loader_options(dataset: cebra.data.Dataset) -> List[str]:
-    """Return all possible dataloaders for the given dataset."""
-
-    loader_options = []
-    if isinstance(dataset, cebra.data.SingleSessionDataset):
-        mixed = True
-        if dataset.continuous_index is not None:
-            loader_options.append(cebra.data.ContinuousDataLoader)
-        else:
-            mixed = False
-        if dataset.discrete_index is not None:
-            loader_options.append(cebra.data.DiscreteDataLoader)
-        else:
-            mixed = False
-        if mixed:
-            loader_options.append(cebra.data.MixedDataLoader)
-    elif isinstance(dataset, cebra.data.MultiSessionDataset):
-        mixed = True
-        if dataset.continuous_index is not None:
-            loader_options.append(cebra.data.ContinuousMultiSessionDataLoader)
-        else:
-            mixed = False
-        if dataset.discrete_index is not None:
-            pass  # not implemented yet
-        else:
-            mixed = False
-        if mixed:
-            pass  # not implemented yet
-    else:
-        raise TypeError(f"Invalid dataset type: {dataset}")
-    return loader_options
-
+import torch
+import numpy as np
+from typing import Union
+import numpy.typing as npt
+import warnings
 
 def download_file_from_url(url: str) -> str:
     """Download a fole from ``url``.
@@ -88,3 +59,47 @@ def download_file_from_zip_url(url, file="montblanc_tracks.h5"):
             except zipfile.error:
                 pass
     return pathlib.Path(foldername) / "data" / file
+
+
+def _is_integer(y: Union[npt.NDArray, torch.Tensor]) -> bool:
+    """Check if the values in ``y`` are :py:class:`int`.
+
+    Args:
+        y: An array, either as a :py:func:`numpy.array` or a :py:class:`torch.Tensor`.
+    
+    Returns:
+        ``True`` if ``y`` contains :py:class:`int`.
+    """
+    return (isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.integer)
+           ) or (isinstance(y, torch.Tensor) and
+                 (not torch.is_floating_point(y) and not torch.is_complex(y)))
+
+
+def _is_floating(y: Union[npt.NDArray, torch.Tensor]) -> bool:
+    """Check if the values in ``y`` are :py:class:`int`.
+    
+    Note: 
+        There is no ``torch`` method to check that the ``dtype`` of a :py:class:`torch.Tensor`
+        is a :py:class:`float`, consequently, we check that it is not :py:class:`int` nor
+        :py:class:`complex`.
+
+    Args:
+        y: An array, either as a :py:func:`numpy.array` or a :py:class:`torch.Tensor`.
+    
+    Returns:
+        ``True`` if ``y`` contains :py:class:`float`.
+    """
+
+    return (isinstance(y, np.ndarray) and
+            np.issubdtype(y.dtype, np.floating)) or (isinstance(
+                y, torch.Tensor) and torch.is_floating_point(y))
+
+def __getattr__(key):
+    if key == "get_loader_options":
+        import cebra.data.helper
+        warnings.warn(
+            "The 'get_loader_options' function has been moved to 'cebra.data.helpers' module. "
+            "Please update your imports.",
+            DeprecationWarning
+        )
+        return cebra.data.helper.get_loader_options
