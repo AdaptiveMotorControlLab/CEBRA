c59ee13cf67c9d89ec4428474d40aa3345722fef
Steffen Schneider <stes@hey.com>
2023-09-13 00:43:29 +0200

    fix version in re-install

--- reinstall.sh
+++ reinstall.sh
@@ -15,7 +15,7 @@ pip uninstall -y cebra
 # Get version info after uninstalling --- this will automatically get the
 # most recent version based on the source code in the current directory.
 # $(tools/get_cebra_version.sh)
-VERSION=0.2.0
+VERSION=0.3.0rc1
 echo "Upgrading to CEBRA v${VERSION}"
 
 # Upgrade the build system (PEP517/518 compatible)
