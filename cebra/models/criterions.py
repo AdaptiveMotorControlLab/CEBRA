f2e73a0383bf81da501246f5697ea53986cf10c6
Mackenzie Mathis <mathis@rowland.harvard.edu>
2023-10-02 21:41:41 +0200

    Update criterions.py
    - seemed redundant.

--- cebra/models/criterions.py
+++ cebra/models/criterions.py
@@ -84,8 +84,7 @@ def infonce(
 
     Note:
         - The behavior of this function changed beginning in CEBRA 0.3.0.
-        The InfoNCE implementation is numerically stabilited. In versions prior
-        to 0.3.0.
+        The InfoNCE implementation is numerically stabilited.
     """
     with torch.no_grad():
         c, _ = neg_dist.max(dim=1, keepdim=True)
