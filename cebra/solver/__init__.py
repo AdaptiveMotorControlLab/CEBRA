02e43f7eaf29eb6b3aacd5dc76d5f8783ee0af4e
Steffen Schneider <stes@hey.com>
2024-01-03 00:32:49 +0100

    Apply updated license headers (#118)
    Apply license headers (#118)
    
    Co-authored-by: CEBRA <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Steffen <steffen@bethgelab.org>
    Co-authored-by: MMathis <mathis@rowland.harvard.edu>

--- cebra/solver/__init__.py
+++ cebra/solver/__init__.py
@@ -1,13 +1,23 @@
 #
-# (c) All rights reserved. ECOLE POLYTECHNIQUE FÉDÉRALE DE LAUSANNE,
-# Switzerland, Laboratory of Prof. Mackenzie W. Mathis (UPMWMATHIS) and
-# original authors: Steffen Schneider, Jin H Lee, Mackenzie W Mathis. 2023.
-#
+# CEBRA: Consistent EmBeddings of high-dimensional Recordings using Auxiliary variables
+# © Mackenzie W. Mathis & Steffen Schneider (v0.4.0+)
 # Source code:
 # https://github.com/AdaptiveMotorControlLab/CEBRA
 #
 # Please see LICENSE.md for the full license document:
-# https://github.com/AdaptiveMotorControlLab/CEBRA/LICENSE.md
+# https://github.com/AdaptiveMotorControlLab/CEBRA/blob/main/LICENSE.md
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 """Variants of CEBRA solvers for single- and multi-session training.
 
--- tests/test_sklearn.py
+++ tests/test_sklearn.py
@@ -1,13 +1,23 @@
 #
-# (c) All rights reserved. ECOLE POLYTECHNIQUE FÉDÉRALE DE LAUSANNE,
-# Switzerland, Laboratory of Prof. Mackenzie W. Mathis (UPMWMATHIS) and
-# original authors: Steffen Schneider, Jin H Lee, Mackenzie W Mathis. 2023.
-#
+# CEBRA: Consistent EmBeddings of high-dimensional Recordings using Auxiliary variables
+# © Mackenzie W. Mathis & Steffen Schneider (v0.4.0+)
 # Source code:
 # https://github.com/AdaptiveMotorControlLab/CEBRA
 #
 # Please see LICENSE.md for the full license document:
-# https://github.com/AdaptiveMotorControlLab/CEBRA/LICENSE.md
+# https://github.com/AdaptiveMotorControlLab/CEBRA/blob/main/LICENSE.md
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 import itertools
 import tempfile
@@ -703,19 +713,19 @@ def test_sklearn_adapt_multisession(model_architecture, device):
         batch_size=42,
         verbose=True,
     )
-    
+
     # example dataset
     Xs = [np.random.uniform(0, 1, (1000, 50)) for i in range(3)]
     ys = [np.random.uniform(0, 1, (1000, 5)) for i in range(3)]
-    
+
     X_new = np.random.uniform(0, 1, (1000, 50))
     y_new = np.random.uniform(0, 1, (1000, 5))
-    
+
     cebra_model.fit(Xs, ys)
-    
+
     with pytest.raises(NotImplementedError, match=".*multisession.*"):
         cebra_model.fit(X_new, y_new, adapt=True)
-    
+
 
 @_util.parametrize_slow(
     arg_names="model_architecture,device,pad_before_transform",
