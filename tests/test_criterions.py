043784b49e38603e4daca85679a372ef8e3bb003
Steffen Schneider <stes@hey.com>
2024-01-10 05:39:25 +0100

    Seed `test_similarities` function to avoid test failure

--- tests/test_criterions.py
+++ tests/test_criterions.py
@@ -259,10 +259,10 @@ def _reference_infonce(pos_dist, neg_dist):
 
 
 def test_similiarities():
-
-    ref = torch.randn(10, 3)
-    pos = torch.randn(10, 3)
-    neg = torch.randn(12, 3)
+    rng = torch.Generator().manual_seed(42)
+    ref = torch.randn(10, 3, generator = rng)
+    pos = torch.randn(10, 3, generator = rng)
+    neg = torch.randn(12, 3, generator = rng)
 
     pos_dist, neg_dist = _reference_dot_similarity(ref, pos, neg)
     pos_dist_2, neg_dist_2 = cebra_criterions.dot_similarity(ref, pos, neg)
