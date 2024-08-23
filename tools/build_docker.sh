b28ce6b41114446505a01bccc19f99d44f1702f3
Steffen Schneider <stes@hey.com>
2024-08-23 23:24:16 +0200

    update build tooling

--- tools/build_docker.sh
+++ tools/build_docker.sh
@@ -16,20 +16,25 @@ echo Building $DOCKERNAME
 
 #docker login <your registry>
 
-docker build \
-	--build-arg UID=$(id -u) \
-	--build-arg GID=$(id -g) \
-	--build-arg GIT_HASH=$(git rev-parse HEAD) \
-       	-t $DOCKERNAME .
-docker tag $DOCKERNAME $LATEST
+if [[ "$1" -ne "dev" ]]; then
+  docker build \
+  	--build-arg UID=$(id -u) \
+  	--build-arg GID=$(id -g) \
+  	--build-arg GIT_HASH=$(git rev-parse HEAD) \
+         	-t $DOCKERNAME .
+  docker tag $DOCKERNAME $LATEST
+  extra_kwargs=()
+else
+  extra_kwargs=( -v .:/local-dev -w /local-dev )
+fi
 
 docker run \
-	--gpus 2 \
-	-v ${CEBRA_DATADIR:-./data}:/data \
-	--env CEBRA_DATADIR=/data \
-	--network host \
-	-it $DOCKERNAME python -m pytest --doctest-modules tests ./docs/source/usage.rst cebra
-
+  --gpus 2 \
+  ${extra_kwargs[@]} \
+  -v ${CEBRA_DATADIR:-./data}:/data \
+  --env CEBRA_DATADIR=/data \
+  --network host \
+  -it $DOCKERNAME python -m pytest --ff -x -m "not requires_dataset" --doctest-modules ./docs/source/usage.rst tests cebra
 
 #docker push $DOCKERNAME
 #docker push $LATEST
